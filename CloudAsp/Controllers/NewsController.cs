83014068288d1b08b1666bc766cd3ba8c4de0b88
Yaroslav Vovk <MasterOfDisaster2495@gmail.com>
2017-07-15 23:27:04 +0300

    Напиши шось

---
+++ CloudAsp/Controllers/NewsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using CloudAsp.Models;
+using CloudAsp.Models.News;
+using CloudAspData;
+using CloudAspData.Entity;
+
+namespace CloudAsp.Controllers
+{
+    public class NewsController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+          
+
+            return View();
+        }
+
+    }
+}
---
+++ CloudAspData/CloudInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudAspData.Entity;
+using CloudAspData.EntityFramework;
+
+namespace CloudAspData
+{
+    public class CloudInitializer: System.Data.Entity.DropCreateDatabaseIfModelChanges<CloudContext>
+    {
+        protected override void Seed(CloudContext context)
+        {
+            if (!context.Roles.Any())
+            {
+                Role admin = new Role{Id = Guid.NewGuid(), Name = "Admin"};
+                Role user = new Role{Id = Guid.NewGuid(), Name = "User"};
+                context.Roles.Add(admin);
+                context.Roles.Add(user);
+                context.Clients.Add(new Client
+                {
+                    Id = Guid.NewGuid(),
+                    Role = admin,
+                    Email = "azaza",
+                    Password = "123",
+                    FirstName = "Sasha"
+                });
+                News news = new News{Id = Guid.NewGuid(), Caption = "TestNews 1", Text = "gjchhbdjkvhndlkvjdl"};
+                context.News.Add(news);
+                context.SaveChanges();
+            }
+
+            base.Seed(context);
+        }
+    }
+}
