da45256a8f6e9bdcdbbd9b943c13d467283433e1
Yaroslav Vovk <MasterOfDisaster2495@gmail.com>
2017-07-19 22:47:18 +0300

    Лох Пидр

--- CloudAsp/Controllers/NewsController.cs
+++ CloudAsp/Controllers/NewsController.cs
@@ -6,6 +6,7 @@ using CloudAsp.Models;
 using CloudAsp.Models.News;
 using CloudAspData;
 using CloudAspData.Entity;
+using HtmlAgilityPack;
 
 namespace CloudAsp.Controllers
 {
@@ -14,7 +15,31 @@ namespace CloudAsp.Controllers
        
         public ActionResult Index()
         {
-          
+            string Url = "https://ria.ru/lenta/";
+            HtmlWeb web = new HtmlWeb();
+
+            HtmlAgilityPack.HtmlDocument doc = web.Load(Url);
+            var node = doc.DocumentNode.SelectSingleNode("//div[@class='b-list']");
+            var nodes = node.ChildNodes;
+
+
+
+
+            foreach (HtmlNode lin in nodes)
+            {
+
+                var mynode = lin.ChildNodes;
+                foreach (HtmlNode aref in mynode)
+                {
+                    string old_url = aref.GetAttributeValue("href", String.Empty) + Environment.NewLine;
+                    string new_url = String.Concat("https://ria.ru/", old_url);
+                    aref.SetAttributeValue("href", new_url);
+                }
+
+            }
+
+
+            ViewBag.HtmlContent =  node.OuterHtml;
 
             return View();
         }
